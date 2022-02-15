b0aa2e633761a4f6695ab4544bb065cab3ca0728
cuiyuan <charlescy1031@icloud.com>
2022-02-15 19:02:17 +0800

    update
    updates

--- handle-themes-file/lib/HandleThemes.ts
+++ handle-themes-file/lib/HandleThemes.ts
@@ -2,19 +2,19 @@
  * @Author: Cuiyuan
  * @Date: 2022-02-09 18:57:46
  * @LastEditors: Cuiyuan
- * @LastEditTime: 2022-02-09 19:02:17
+ * @LastEditTime: 2022-02-09 19:09:07
  * @Description: file content
- * @FilePath: /未命名文件夹/handle-themes-file/lib/HandleThemes.ts
+ * @FilePath: /serverTests/handle-themes-file/lib/HandleThemes.ts
  */
-import {readdir} from "fs";
+import { readdir } from "fs";
 export default class HandleThemes {
-    public getFolderFiles(path:string):void{
-        readdir(path,(errStatus: null,fileList: any)=>{
-            if(errStatus!==null){
-                console.log("文件读取失败，错误原因:",errStatus)
-                return 
+    public getFolderFiles(path: string): void {
+        readdir(path, (errStatus: any, fileList: any) => {
+            if (errStatus !== null) {
+                console.log("文件读取失败，错误原因:", errStatus);
+                return;
             }
-            console.log("文件读取成功",fileList)
-        })
+            console.log("文件读取成功", fileList);
+        });
     }
 }
